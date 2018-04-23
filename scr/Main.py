dea55b0b7e4014f59a92e3f29fc411b47705a3d3
saddenlar <saddenlar@163.com>
2018-04-22 18:45:00 -0700

    Main is upgraded to run on Pi

--- scr/Main.py
+++ scr/Main.py
@@ -1,54 +1,59 @@
 '''
 Created on Apr 15, 2018
 
-A face detection program that use basic function in OpenCV3
+A face detection program that use basic function in OpenCV3 on Pi3
 
 It's a practice
 
 @author: sadde
 '''
-import cv2 as cv
+
+import io
+from time import sleep
+import picamera
 import numpy as np
-import matplotlib.pyplot as plt
+import cv2
+
+
 
 if __name__ == '__main__':
-    face_cascade = cv.CascadeClassifier('C:\Program Files\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
-    eye_cascade = cv.CascadeClassifier('C:\Program Files\opencv\sources\data\haarcascades\haarcascade_eye.xml')
+    face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.4.0/data/haarcascades/haarcascade_frontalface_alt.xml')
+    eye_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.4.0/data/haarcascades/haarcascade_eye.xml')
     
-    video_capture = cv.VideoCapture(0)
-    
-    while True:
-        ret, frame = video_capture.read()
-        
-        img = frame
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        
-        '''
-        cv.namedWindow('img')
-        cv.imshow('img', img)
-        cv.waitKey(0)
-        
-        cv.namedWindow('grey')
-        cv.imshow('grey', grey)
-        cv.waitKey(0)
-        '''
-        
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        for (x,y,w,h) in faces:
-            cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = img[y:y+h, x:x+w]
-            eyes = eye_cascade.detectMultiScale(roi_gray)
-            for (ex,ey,ew,eh) in eyes:
-                cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-        
-        cv.namedWindow('img')
-        cv.imshow('Video', img)
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    video_capture.release()
-    cv.destroyAllWindows()    
+    with picamera.PiCamera() as camera:
+	camera.resolution = (320,240)
+	# camera.framerate = 24
+	sleep(1)
+	
+	stream = io.BytesIO()
+	for foo in camera.capture_continuous(stream, format='jpeg', use_video_port=True):
+		data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+		image = cv2.imdecode(data, cv2.IMREAD_COLOR)
+		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		
+		cv2.imshow('gray',gray)
+		# detect faces and get coordinations
+		faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+		
+		# detect eyes based on face detection results
+		for (x,y,w,h) in faces:
+            		cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+            		roi_gray = gray[y:y+h, x:x+w]
+            		roi_color = image[y:y+h, x:x+w]
+            		eyes = eye_cascade.detectMultiScale(roi_gray)
+            		for (ex,ey,ew,eh) in eyes:
+                		cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+		
+		cv2.imshow('img', image)
+		
+		# Truncate the stream to the current position
+		# in case prior interation output a longer image
+		
+		stream.truncate()
+		stream.seek(0)
+		
+		if cv2.waitKey(1) & 0xFF == ord('q'): break
+  
     
     
     
