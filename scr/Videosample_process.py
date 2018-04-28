4c4db2ffb38fdf27a87188f0f8aa41ebcf874a96
Jay <saddenlar@163.com>
2018-04-27 19:50:59 -0700

    Delete Videosample_process.py

--- scr/Videosample_process.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-with cv2.VideoCapture("video.avi") as capture:
-	while True:
-		ret, img = capture.read()
-		result = img#processFrame(img)
-		cv2.imshow('some', result)
-    		if cv2.waitKey(1) & 0xFF == ord('q'):break
-cv2.destroyAllWindows()
