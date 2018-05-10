10d393f1dcfa0ee6bda51f1ab6929b28b5eb351b
saddenlar <saddenlar@163.com>
2018-05-09 18:06:01 -0700

    web pages

---
+++ scr/head-track/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/saddenlar/pen/aGqObK.
+
+ This progress bar uses the HTML5 custom data-* attribute to allow for quick updating to a progress bar animated by Zepto (or jQuery). The animation is wrapped in a window.resize function to reanimate if the browser size is changed.
---
+++ scr/head-track/js/index.js
@@ -0,0 +1,44 @@
+// on page load...
+moveProgressBar();
+// on browser resize...
+$(window).resize(function() {
+  moveProgressBar();
+});
+
+// SIGNATURE PROGRESS
+function moveProgressBar() {
+  console.log("moveProgressBar");
+  var getPercent = ($('.progress-wrap').data('progress-percent') / 100);
+  var getProgressWrapWidth = $('.progress-wrap').width();
+  var progressTotal = getPercent * getProgressWrapWidth;
+  var animationLength = 2500;
+
+  // on page load, animate percentage bar to data percentage length
+  // .stop() used to prevent animation queueing
+  $('.progress-bar').stop().animate({
+    left: progressTotal
+  }, animationLength);
+}
+
+//the function is called when user asked the robot if he is happy
+function sendAnnotationRequest()
+{
+  /*This is the URL of the image to be annotated*/
+  var annImgURL = getUserFaceUrl(); // url of the image to be annotated
+  
+  /*This is the URL for the web service request, including the key of your app as a parameter*/
+  var url ="127.0.0.1";
+  
+  /*The is the request that will be sent as JSON*/
+  var request = {
+  };
+  ajaxRequest("POST", url, handleResponse, JSON.stringify(request));
+}
+
+//when google api response we parse the text and process
+function handleResponse() {
+  if (successfulRequest(this)) {
+    var response = JSON.parse(this.responseText);
+    console.log(response)
+  }
+}
